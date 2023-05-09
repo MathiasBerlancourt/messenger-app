26735eff69903d2ef11cec261abb627ee3395e25
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-05-09 21:50:22 +0200

    project init

---
+++ src/app/(site)/components/AuthForm.tsx
@@ -0,0 +1,9 @@
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
